75de5cba5629d04dbd83df7daa7bb6158430a8d6
Mustafa Ay <mustafaay33.44@gmail.com>
2022-08-16 22:48:51 +0300

    git ignore 1

---
+++ src/deneme.java
@@ -0,0 +1,14 @@
+public class deneme {
+
+    public static void main(String[] args) {
+        System.out.println("merhaba");
+        System.out.println("merhaba");
+        System.out.println("merhaba");
+        System.out.println("merhaba");
+        System.out.println("merhaba");
+        System.out.println("merhaba");
+        System.out.println("merhaba");
+        System.out.println("merhaba");
+        System.out.println("merhaba");
+    }
+}
---
+++ src/hh.java
@@ -0,0 +1,15 @@
+public class hh {
+
+
+    public static void main(String[] args) {
+        System.out.println("5526439772");
+        System.out.println("5526439772");
+        System.out.println("5526439772");
+        System.out.println("5526439772");
+        System.out.println("5526439772");
+        System.out.println("5526439772");
+        System.out.println("5526439772");
+        System.out.println("5526439772");
+        System.out.println("5526439772");
+    }
+}
